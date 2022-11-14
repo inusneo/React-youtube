05a2abff2b2da6f30c575d784ac780dfcdf0a39d
Kim Yeongjin <inusneo@gmail.com>
2022-11-14 14:43:47 +0900

    Updates

---
+++ precache-manifest.93d2ba8b0b7f980a5215331dde9beff6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "81f4240906555742d49ef6e2b863e2a3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "70772149a6083a77a3be",
+    "url": "/static/css/main.fd8b8e6a.chunk.css"
+  },
+  {
+    "revision": "d6ff72a9b04176138b01",
+    "url": "/static/js/2.d10d3e07.chunk.js"
+  },
+  {
+    "revision": "e8b0ae1bd2f59a85f300b3c60c50941a",
+    "url": "/static/js/2.d10d3e07.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "70772149a6083a77a3be",
+    "url": "/static/js/main.7cea811f.chunk.js"
+  },
+  {
+    "revision": "bae492c776d6e417a041",
+    "url": "/static/js/runtime-main.1b461360.js"
+  }
+]);
