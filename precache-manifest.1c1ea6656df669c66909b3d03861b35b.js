e961d0f30b8db7c146262511de9c1245209b41a6
Kim Yeongjin <inusneo@gmail.com>
2022-11-18 12:14:39 +0900

    Updates

---
+++ precache-manifest.1c1ea6656df669c66909b3d03861b35b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "545a0848c8a252a9394e14773709fc19",
+    "url": "/React-youtube/index.html"
+  },
+  {
+    "revision": "c62a82107dbddcc9d543",
+    "url": "/React-youtube/static/css/main.27d9cb5d.chunk.css"
+  },
+  {
+    "revision": "dc751cec86c92e877359",
+    "url": "/React-youtube/static/js/2.1812bd7c.chunk.js"
+  },
+  {
+    "revision": "2d6cb2fe842a8d70be7fceeef4f6dfa4",
+    "url": "/React-youtube/static/js/2.1812bd7c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c62a82107dbddcc9d543",
+    "url": "/React-youtube/static/js/main.bcc6f005.chunk.js"
+  },
+  {
+    "revision": "725a43a30e41a60c8b96",
+    "url": "/React-youtube/static/js/runtime-main.ab4197b7.js"
+  }
+]);
